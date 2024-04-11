d135cd8993ffca6a610c871b26e7cbc70d5eba50
Juan Manuel De La Torre Sanchez <jdela@unal.edu.co>
2024-04-11 02:55:47 -0500

    Commenting awsprofile

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -6,8 +6,7 @@ terraform {
     region                   = "us-east-1"
     dynamodb_table           = "TerraformLockState"
     # Uncomment the following lines to use AWS shared credentials 
-
-    shared_credentials_files = ["~/.aws/credentials"]
-    profile                  = "terraform-user"
+    # shared_credentials_files = ["~/.aws/credentials"]
+    # profile                  = "terraform-user"
   }
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,6 +10,6 @@ terraform {
 provider "aws" {
   region                   = "us-east-1"
   # Uncomment the following lines to use AWS shared credentials 
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "terraform-user"
+  # shared_credentials_files = ["~/.aws/credentials"]
+  # profile                  = "terraform-user"
 }
